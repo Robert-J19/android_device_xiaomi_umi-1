4c39c100539ad905ff732fd4a336901cce4147e0
Robert <75765385+Robert-J19@users.noreply.github.com>
2021-10-04 03:46:37 +0800

    WaveOS

--- wave_umi.mk
+++ wave_umi.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/umi/device.mk)
 
 # Inherit from the Lineage configuration.
-$(call inherit-product, vendor/lineage/configs/common.mk)
+$(call inherit-product, vendor/wave/configs/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_umi
+PRODUCT_NAME := wave_umi
 PRODUCT_DEVICE := umi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10
